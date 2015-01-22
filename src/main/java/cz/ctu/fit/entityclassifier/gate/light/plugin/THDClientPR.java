ee4600547dceb96645026969235e75cf780a0893
Milan Dojchinovski <dojcinovski.milan@gmail.com>
2015-01-22 17:55:54 +0100

    init code imported

---
+++ src/main/java/cz/ctu/fit/entityclassifier/gate/light/plugin/THDClientPR.java
@@ -0,0 +1,198 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.ctu.fit.entityclassifier.gate.light.plugin;
+
+import gate.Corpus;
+import gate.Document;
+import gate.ProcessingResource;
+import gate.Resource;
+import gate.creole.AbstractLanguageAnalyser;
+import gate.creole.ExecutionException;
+import gate.creole.ResourceInstantiationException;
+
+/**
+ * @author Milan Dojchinovski
+ * <milan (at) dojchinovski (dot) mk>
+ * Twitter: @m1ci
+ * www: http://dojchinovski.mk
+ */
+public class THDClientPR extends AbstractLanguageAnalyser implements ProcessingResource {
+
+    private String apikey;
+    private String outputASName;
+    private String annotationsName;
+    private String dbpediaOntologyLocation;
+    private String knowledgeBase;
+    private String entityType;
+    private String lang;
+    private String provenance;
+    private String longEntityLinking;
+    
+    public THDClientPR() {
+    }
+
+    @Override
+    public Resource init() throws ResourceInstantiationException {
+        System.out.println("THD Client was successfully inicialized.");
+        return super.init();
+    }
+    
+    @Override
+    public void reInit() throws ResourceInstantiationException {
+        init();
+    }
+        
+    @Override
+    public void execute() throws ExecutionException {
+        Document doc = getDocument();
+        TextProcessor.getInstance().processDocument(
+                doc,
+                apikey,
+                outputASName,
+                annotationsName,
+                knowledgeBase,
+                entityType,
+                lang,
+                provenance,
+                longEntityLinking);
+    }
+    
+    @Override
+    public Corpus getCorpus() {
+        return corpus;
+    }
+
+    @Override
+    public void setCorpus(Corpus corpus) {
+        this.corpus = corpus;
+    }
+
+    /**
+     * @return the apikey
+     */
+    public String getApikey() {
+        return apikey;
+    }
+
+    /**
+     * @param apikey the apikey to set
+     */
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    /**
+     * @return the outputASName
+     */
+    public String getOutputASName() {
+        return outputASName;
+    }
+
+    /**
+     * @param outputASName the outputASName to set
+     */
+    public void setOutputASName(String outputASName) {
+        this.outputASName = outputASName;
+    }
+
+    /**
+     * @return the annotationsName
+     */
+    public String getAnnotationsName() {
+        return annotationsName;
+    }
+
+    /**
+     * @param annotationsName the annotationsName to set
+     */
+    public void setAnnotationsName(String annotationsName) {
+        this.annotationsName = annotationsName;
+    }
+
+    /**
+     * @return the dbpediaOntologyLocation
+     */
+    public String getDbpediaOntologyLocation() {
+        return dbpediaOntologyLocation;
+    }
+
+    /**
+     * @param dbpediaOntologyLocation the dbpediaOntologyLocation to set
+     */
+    public void setDbpediaOntologyLocation(String dbpediaOntologyLocation) {
+        this.dbpediaOntologyLocation = dbpediaOntologyLocation;
+    }
+
+    /**
+     * @return the knowledgeBase
+     */
+    public String getKnowledgeBase() {
+        return knowledgeBase;
+    }
+
+    /**
+     * @param knowledgeBase the knowledgeBase to set
+     */
+    public void setKnowledgeBase(String knowledgeBase) {
+        this.knowledgeBase = knowledgeBase;
+    }
+
+    /**
+     * @return the entityType
+     */
+    public String getEntityType() {
+        return entityType;
+    }
+
+    /**
+     * @param entityType the entityType to set
+     */
+    public void setEntityType(String entityType) {
+        this.entityType = entityType;
+    }
+
+    /**
+     * @return the lang
+     */
+    public String getLang() {
+        return lang;
+    }
+
+    /**
+     * @param lang the lang to set
+     */
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    /**
+     * @return the provenance
+     */
+    public String getProvenance() {
+        return provenance;
+    }
+
+    /**
+     * @param provenance the provenance to set
+     */
+    public void setProvenance(String provenance) {
+        this.provenance = provenance;
+    }
+
+    /**
+     * @return the longEntityLinking
+     */
+    public String getLongEntityLinking() {
+        return longEntityLinking;
+    }
+
+    /**
+     * @param longEntityLinking the longEntityLinking to set
+     */
+    public void setLongEntityLinking(String longEntityLinking) {
+        this.longEntityLinking = longEntityLinking;
+    }
+}
---
+++ src/main/java/cz/ctu/fit/entityclassifier/gate/light/plugin/TextProcessor.java
@@ -0,0 +1,163 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.ctu.fit.entityclassifier.gate.light.plugin;
+
+import gate.AnnotationSet;
+import gate.Document;
+import gate.FeatureMap;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ *
+ * @author Milan
+ */
+public class TextProcessor {
+    
+    private static TextProcessor textProcessor = null;
+    
+    public static TextProcessor getInstance(){
+        if(textProcessor == null){
+            textProcessor = new TextProcessor();
+        }
+        return textProcessor;
+    }
+    
+    public void processDocument(
+            Document doc,
+            String apikey,
+            String outputASName,
+            String annotationsName,
+            String knowledgeBase,
+            String entityType,
+            String lang,
+            String provenance,
+            String longEntityLinking){
+        
+        try {
+
+            String path = "http://ner.vse.cz/thd/api/v2/extraction?apikey="+apikey+"&format=xml";
+
+            if(knowledgeBase != null){
+                path += "&knowledge_base="+knowledgeBase;
+            }
+            if(entityType != null){
+                path += "&entity_type="+entityType;
+            }
+            if(lang != null){
+                path += "&lang="+lang;
+            }
+            if(provenance != null){
+                path += "&provenance="+provenance;
+            }
+            if(longEntityLinking != null){
+                path += "&priority_entity_linking="+longEntityLinking;
+            }
+
+            URL url = new URL(path);
+
+            StringBuffer buffer = new StringBuffer();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-type", "text/plain; charset=UTF-8");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            
+            OutputStream os = connection.getOutputStream();
+            os.write(doc.getContent().toString().getBytes("UTF-8"));
+            os.flush();
+            os.close();
+
+            InputStream is = connection.getInputStream();
+            Reader isr = new InputStreamReader(is,"UTF-8");
+            Reader in = new BufferedReader(isr);
+            int ch;
+
+            while ((ch = in.read()) > -1) {
+                buffer.append((char) ch);
+            }
+            in.close();
+            isr.close();
+            String result = buffer.toString();
+//            System.out.println(result);
+            AnnotationSet thdAnnots = doc.getAnnotations(outputASName);
+            
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            org.w3c.dom.Document xmldoc = (org.w3c.dom.Document) dBuilder.parse(new ByteArrayInputStream(result.getBytes()));
+            NodeList eList = xmldoc.getElementsByTagName("entity");
+                    
+            for (int i = 0; i < eList.getLength(); i++) {
+                
+                Node nEntity = eList.item(i);
+
+                if (nEntity.getNodeType() == Node.ELEMENT_NODE) {
+                            
+                    Element entity = (Element)nEntity;
+                    String foundEntityType = entity.getElementsByTagName("entityType").item(0).getTextContent();
+                    long startOffset = Long.parseLong(entity.getElementsByTagName("startOffset").item(0).getTextContent());
+                    long endOffset = Long.parseLong(entity.getElementsByTagName("endOffset").item(0).getTextContent());
+                    NodeList underlyingStringObj = entity.getElementsByTagName("underlyingString");
+                    
+                    if(underlyingStringObj.getLength() > 0) {
+                        
+                        String underlyingString = underlyingStringObj.item(0).getTextContent();
+                        NodeList tList = entity.getElementsByTagName("type");
+                        FeatureMap fm = gate.Factory.newFeatureMap();
+
+                        for (int j = 0; j < tList.getLength(); j++) {
+                            
+                            Node nType = tList.item(j);
+
+                            if (nType.getNodeType() == Node.ELEMENT_NODE) {
+
+                                Element type = (Element)nType;
+                                Node entityLabel = type.getElementsByTagName("entityLabel").item(0);
+                                Node entityURI = type.getElementsByTagName("entityURI").item(0);
+
+                                if(entityLabel != null && entityURI != null) {
+
+                                    fm.put("entity", entityLabel.getTextContent());
+                                    fm.put("entityURI", entityURI.getTextContent());
+                                    fm.put("entityType", foundEntityType);
+                                }
+
+                                Node typeLabel = type.getElementsByTagName("typeLabel").item(0);
+                                Node typeURI = type.getElementsByTagName("typeURI").item(0);
+
+                                if(typeLabel != null && typeURI != null) {
+                                        
+                                    fm.put("typeLabel", typeLabel.getTextContent());
+                                    fm.put("typeURI", typeURI.getTextContent());
+                                    fm.put("entityType", foundEntityType);
+                                }
+                            }
+                        }
+                        thdAnnots.add(startOffset, endOffset, annotationsName, fm);
+                    } else {
+                        System.out.println("Underlying string not present");
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(TextProcessor.class.getName()).log(Level.SEVERE, null, ex);        
+        }
+    }
+}
